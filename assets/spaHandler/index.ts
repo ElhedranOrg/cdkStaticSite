2041e727d31983ef22dee4e49c35b32fe59e5450
Ian Walters <ian.walters@roames.com.au>
2020-04-23 13:57:27 +1000

    tweak edge code and bump version

--- assets/spaHandler/index.ts
+++ assets/spaHandler/index.ts
@@ -3,8 +3,8 @@ import * as Lambda from 'aws-lambda';
 export const handler: Lambda.CloudFrontRequestHandler = (event, context, callback) => {
     const request = event.Records[0].cf.request;
     if (request.uri.indexOf('.') === -1) {
+        console.log(`Changing request path from "${request.uri}" to "/index.html"`);
         request.uri = '/index.html'
     }
-    console.log(`Request uri set to "${request.uri}"`);
     callback(null, request);
 }
